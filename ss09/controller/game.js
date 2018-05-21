cd933ed1b221ac7b025aacc13da3093300abd90d
Edward Jung <edwardjung@Edwards-MacBook-Pro.local>
2018-05-20 22:03:39 -0700

    com

--- ss09/controller/game.js
+++ ss09/controller/game.js
@@ -1,15 +1,20 @@
-var game091;
-(function (game091) {
+var game092;
+(function (game092) {
     var Game = /** @class */ (function () {
         function Game() {
             this.board = document.querySelector("#board");
             this.ctx = this.board.getContext('2d');
             this.update = this.update.bind(this);
             this.start = this.start.bind(this);
+            this.handleKeyDown = this.handleKeyDown.bind(this);
             this.stop = this.stop.bind(this);
+            window.addEventListener('keydown', this.handleKeyDown);
             document.querySelector("#btnStart").addEventListener('click', this.start);
             document.querySelector("#btnStop").addEventListener('click', this.stop);
         }
+        Game.prototype.handleKeyDown = function (ev) {
+            this.keyCode = ev.keyCode;
+        };
         Game.prototype.start = function () {
             if (this.isStarted) {
                 return;
@@ -27,7 +32,7 @@ var game091;
         };
         Game.prototype.update = function () {
             this.ctx.clearRect(0, 0, this.board.width, this.board.height);
-            this.player.update();
+            this.player.update(this.keyCode);
             this.player.draw(this.ctx);
         };
         return Game;
@@ -41,22 +46,51 @@ var game091;
             this.color = color;
             this.dx = dx;
             this.dy = dy;
+            this.dx = 0;
+            this.dy = 0;
         }
         RectPlayer.prototype.draw = function (ctx) {
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.w, this.h);
         };
-        RectPlayer.prototype.update = function () {
-            if (this.x < 0 || this.x > 900) {
-                this.dx *= -1;
+        RectPlayer.prototype.update = function (keyCode) {
+            if (keyCode == 37) {
+                if (this.x < 0) {
+                    this.dx = 1;
+                    this.dy = 0;
+                }
+                else {
+                    this.dx = -1;
+                    this.dy = 0;
+                }
+            }
+            else if (keyCode == 38) {
+                this.dx = 0;
+                this.dy = -1;
+            }
+            else if (keyCode == 39) {
+                if (this.x > 990) {
+                    this.dx = -1;
+                    this.dy = 0;
+                }
+                else {
+                    this.dx = 1;
+                    this.dy = 0;
+                }
+            }
+            else if (keyCode == 40) {
+                this.dx = 0;
+                this.dy = 1;
             }
             this.x += this.dx;
             if (this.y < 0 || this.y > 900) {
                 this.dy *= -1;
+                this.dx *= 1;
             }
+            this.x += this.dx;
             this.y += this.dy;
         };
         return RectPlayer;
     }());
     var game = new Game();
-})(game091 || (game091 = {}));
+})(game092 || (game092 = {}));
--- ss09/controller/game.ts
+++ ss09/controller/game.ts
@@ -1,4 +1,4 @@
-namespace game091{
+namespace game092{
 
     class Game{
         public board: HTMLCanvasElement;
@@ -6,15 +6,20 @@ namespace game091{
         public player:RectPlayer;
         private gameloop: any;
         private isStarted:boolean;
-
+        private keyCode: number;
         constructor(){
             this.board = document.querySelector("#board");
             this.ctx = this.board.getContext('2d');
             this.update = this.update.bind(this);
             this.start = this.start.bind(this);
+            this.handleKeyDown = this.handleKeyDown.bind(this);
             this.stop = this.stop.bind(this);
+            window.addEventListener('keydown',this.handleKeyDown);
             document.querySelector("#btnStart").addEventListener('click',this.start);
             document.querySelector("#btnStop").addEventListener('click' ,this.stop);
+        }
+        public handleKeyDown(ev: KeyboardEvent){
+            this.keyCode = ev.keyCode;
         }
         public start(){
             if (this.isStarted){
@@ -34,7 +39,7 @@ namespace game091{
         }
         public update(){
             this.ctx.clearRect(0,0,this.board.width,this.board.height);
-            this.player.update();
+            this.player.update(this.keyCode);
             this.player.draw(this.ctx);
 
         }
@@ -42,22 +47,55 @@ namespace game091{
     }
     class RectPlayer{
          constructor(public x:number, public y:number,public w:number,public h:number,public color:string,public dx:number,public dy:number){
+             this.dx = 0;
+             this.dy = 0;
          }
          public draw(ctx:CanvasRenderingContext2D){
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x,this.y,this.w,this.h);
          }
-         public update(){
-             if (this.x < 0 || this.x > 900){
-                 this.dx *=-1;
+         public update(keyCode:number){
+             if (keyCode == 37){
+                if (this.x < 0){
+                    this.dx = 1;
+                    this.dy = 0;
+                    
+                }
+                else{
+                    this.dx = -1;
+                    this.dy = 0;
+                }
              }
-             this.x += this.dx;
-             if (this.y < 0 || this.y > 900){
-                this.dy *=-1;
+             else if (keyCode == 38){
+                this.dx = 0;
+                this.dy = -1;
+             }
+             else if (keyCode == 39){
+                if (this.x > 990){
+                    this.dx = -1;
+                    this.dy = 0;
+                    
+                }
+                else{
+                    this.dx = 1;
+                    this.dy = 0;
+                }
+                
+             }
+             else if (keyCode == 40){
+                 this.dx = 0;
+                 this.dy = 1;
+             }
+            
+            this.x += this.dx;
+            if (this.y < 0 || this.y > 900){
+               this.dy *=-1;
+               this.dx *= 1;
             }
-            this.y += this.dy;
-
+             this.x += this.dx;
+             this.y += this.dy;
          }
+         
     }
     
     var game = new Game();
